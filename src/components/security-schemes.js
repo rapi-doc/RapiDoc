b2e4d662e81639398f078fb7cd8b9e90e65f257e
Mrin <mrin9@yahoo.com>
2019-05-09 09:46:38 -0700

    support for multiple api-key

--- src/components/security-schemes.js
+++ src/components/security-schemes.js
@@ -42,8 +42,8 @@ export default class SecuritySchemes extends LitElement {
           <td>
             ${this.schemes[s].type==='apiKey'?html`
               Send <code>'${this.schemes[s].name}'</code> in <code>'${this.schemes[s].in}'</code> with the given value
-              <div class="api-key" data-type="${this.schemes[s].type}" data-in="${this.schemes[s].in}" data-name="${this.schemes[s].name}" style="margin:5px 0" spellcheck="false" >
-                <input type="text" name="token" style="width:202px;" placeholder="api-token" >
+              <div class="api-key" data-type="${this.schemes[s].type}" data-in="${this.schemes[s].in}" data-name="${this.schemes[s].name}" data-inputname="${s}" style="margin:5px 0" spellcheck="false" >
+                <input type="text" name="${s}-token" style="width:202px;" placeholder="api-token" >
                 <button 
                   class="m-btn" 
                   data-action="${this.keyValue?'CLEAR':'SET'}" 
@@ -54,8 +54,8 @@ export default class SecuritySchemes extends LitElement {
             ${this.schemes[s].type==='http' && this.schemes[s].scheme==='basic'?html`
               Send <code>'Authorization'</code> in header which will contains the word  <code>'Basic'</code> followed by a space and a base64-encoded string username:password.
               <div class="api-key " data-type="${this.schemes[s].type}" data-scheme="${this.schemes[s].scheme}" data-in="header" data-name="Authorization" style="margin:15px 0">
-                <input type="text" name="username" style="width:100px;" placeholder="username" spellcheck="false">
-                <input type="text" name="password" style="width:100px;" placeholder="password" spellcheck="false">
+                <input type="text" name="${s}-username" style="width:100px;" placeholder="username" spellcheck="false">
+                <input type="text" name="${s}-password" style="width:100px;" placeholder="password" spellcheck="false">
                 <button 
                   class="m-btn" 
                   data-action="${this.keyValue?'CLEAR':'SET'}" 
@@ -66,7 +66,7 @@ export default class SecuritySchemes extends LitElement {
             ${this.schemes[s].type==='http' && this.schemes[s].scheme==='bearer'?html`
               Send <code>'Authorization'</code> in header which will contains the word  <code>'Bearer'</code> followed by a space and a Token String.
               <div class="api-key" data-type="${this.schemes[s].type}" data-scheme="${this.schemes[s].scheme}" data-in="header" data-name="Authorization" style="margin:15px 0">
-                <input type="text" name="token" style="width:202px;" placeholder="api-token" spellcheck="false" >
+                <input type="text" name="${s}-bearer-token" style="width:202px;" placeholder="api-token" spellcheck="false" >
                 <button 
                   class="m-btn" 
                   data-action="${this.keyValue?'CLEAR':'SET'}" 
@@ -81,8 +81,8 @@ export default class SecuritySchemes extends LitElement {
                 ${this.schemes[s].flows[f].tokenUrl?html`<div><b>Token URL:</b> <code class="url"> ${this.schemes[s].flows[f].tokenUrl}</code></div>`:``}
                 ${this.schemes[s].flows[f].refreshUrl?html`<div><b>Refresh URL:</b> <code class="url"> ${this.schemes[s].flows[f].refreshUrl}</code></div>`:``}
                 <div class="oauth" style="margin:5px 0">
-                  <input type="text" name="client" style="width:100px;" placeholder="client-id" spellcheck="false" >
-                  <input type="text" name="secret" style="width:100px;" placeholder="client-secret" spellcheck="false" >
+                  <input type="text" name="${s}-client" style="width:100px;" placeholder="client-id" spellcheck="false" >
+                  <input type="text" name="${s}-secret" style="width:100px;" placeholder="client-secret" spellcheck="false" >
                 </div>
               `)}
               </div>
@@ -103,24 +103,29 @@ export default class SecuritySchemes extends LitElement {
 
   dispatchChange(e){
     let apiEl = e.target.closest(".api-key");
+    debugger
     if (!apiEl){
       return;
     }
 
-    let keyType  = apiEl.dataset.type;
+    let keyType     = apiEl.dataset.type;
     let keyLocation = apiEl.dataset.in;
-    let keyName  = apiEl.dataset.name;
+    let keyName     = apiEl.dataset.name;
+    let inputname   = apiEl.dataset.inputname;
+
     
     if (e.target.dataset.action === "CLEAR"){
       this.keyValue = "";
-      let tokenEl = apiEl.querySelector("input[name=token]");
+      let tokenEl = apiEl.querySelector(`input[name=${inputname}-token]`);
+
       if (tokenEl){
         tokenEl.value="";
       }
     }
     else{
       if (keyType==="apiKey"){
-        let tokenEl = apiEl.querySelector("input[name=token]");
+        // let tokenEl = apiEl.querySelector("input[name=token]");
+        let tokenEl = apiEl.querySelector(`input[name=${inputname}-token]`);
         if (tokenEl){
           this.keyValue = tokenEl.value;
         }
@@ -129,15 +134,19 @@ export default class SecuritySchemes extends LitElement {
       else if (keyType==="http"){
         let securityScheme=apiEl.dataset.scheme;
         if (securityScheme==="basic"){
-          let userNameEl = apiEl.querySelector("input[name=username]");
-          let passwordEl = apiEl.querySelector("input[name=password]");
+          // let userNameEl = apiEl.querySelector("input[name=username]");
+          // let passwordEl = apiEl.querySelector("input[name=password]");
+          let userNameEl = apiEl.querySelector(`input[name=${inputname}-username]`);
+          let passwordEl = apiEl.querySelector(`input[name=${inputname}-password]`);
+
           if (userNameEl && passwordEl){
             this.keyValue = 'Basic '+ btoa(userNameEl.value+":"+passwordEl.value);
           }
     
         }
         else if (securityScheme==="bearer"){
-          let tokenEl = apiEl.querySelector("input[name=token]");
+          // let tokenEl = apiEl.querySelector("input[name=token]");
+          let tokenEl = apiEl.querySelector(`input[name=${inputname}-bearer-token]`);
           if (tokenEl){
             this.keyValue = "Bearer " + tokenEl.value;
           }
