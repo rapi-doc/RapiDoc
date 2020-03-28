818a5547605c5eddec998b321e87b74c35909b63
Mrin <mrin9@yahoo.com>
2020-03-28 14:57:59 -0700

    generated build for 7.5.0

--- docs/rapidoc-min.js
+++ docs/rapidoc-min.js
@@ -1,5 +1,5 @@
 /*!
- * RapiDoc 7.4.3 - WebComponent to View OpenAPI docs
+ * RapiDoc 7.5.0 - WebComponent to View OpenAPI docs
  * License: MIT
  * Repo   : https://github.com/mrin9/RapiDoc
  * Author : Mrinmoy Majumdar
