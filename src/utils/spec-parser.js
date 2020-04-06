e7f33abc3dd5e7e1742eb808914e31c0295bd5d8
Fredrick Bäcker <fredrick.backer@gmail.com>
2020-04-06 07:16:02 +0200

    remove wrong variable
    is for another PR

--- src/utils/spec-parser.js
+++ src/utils/spec-parser.js
@@ -302,7 +302,6 @@ function groupByTags(openApiSpec, sortTags = false, sortEndpointsBy, allowDuplic
               show: true,
               name: tag,
               description: tagDescr ? tagDescr.description : '',
-              expanded: tagDescr ? tagDescr.expanded : true,
               paths: [],
             };
             tags.push(tagObj);
