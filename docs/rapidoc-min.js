0a12c77a37b8a49c6036bcbf2b72477e8c38e5a8
Mrinmoy <mrin9@yahoo.com>
2021-02-16 18:07:26 -0500

    generated build for 8.4.8

--- docs/rapidoc-min.js
+++ docs/rapidoc-min.js
@@ -1,5 +1,5 @@
 /*!
- * RapiDoc 8.4.6 - WebComponent to View OpenAPI docs
+ * RapiDoc 8.4.8 - WebComponent to View OpenAPI docs
  * License: MIT
  * Repo   : https://github.com/mrin9/RapiDoc
  * Author : Mrinmoy Majumdar
