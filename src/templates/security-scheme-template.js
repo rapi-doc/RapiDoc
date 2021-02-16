c233c47b5a154ebca88b38026d0f5035c7423954
Mrinmoy <mrin9@yahoo.com>
2021-02-15 21:55:32 -0500

    fixed a typo to correctly provide id as  for authentication section

--- src/templates/security-scheme-template.js
+++ src/templates/security-scheme-template.js
@@ -270,7 +270,7 @@ export default function securitySchemeTemplate() {
     return;
   }
   return html`
-  <div id='authentication' style="margin-top:24px; margin-bottom:24px;" class = 'observe-me ${'read focused'.includes(this.renderStyle) ? 'section-gap--read-mode' : 'section-gap '}'>
+  <div id='auth' style="margin-top:24px; margin-bottom:24px;" class = 'observe-me ${'read focused'.includes(this.renderStyle) ? 'section-gap--read-mode' : 'section-gap '}'>
     <div class='sub-title regular-font'> AUTHENTICATION </div>
 
     <div class="small-font-size" style="display:flex; align-items: center; min-height:30px">
