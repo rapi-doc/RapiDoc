0759b0c948c2788efc6405df0092d9422069d28b
Mrinmoy <mrin9@yahoo.com>
2021-02-20 22:16:42 -0500

    short summary

--- src/utils/spec-parser.js
+++ src/utils/spec-parser.js
@@ -324,7 +324,7 @@ function groupByTags(openApiSpec, sortTags = false, sortEndpointsBy) {
           // Generate a short summary which is broken
           let shortSummary = (fullPath.summary || fullPath.description || `${methodName.toUpperCase()} ${path}`).trim();
           if (shortSummary.length > 100) {
-            shortSummary = shortSummary.split('/\r?\n|[.]/')[0]; // take the first line (period or carriage return)
+            shortSummary = shortSummary.split(/[.|!|?]\s|[\r?\n]/)[0]; // take the first line (period or carriage return)
           }
           // Merge Common Parameters with This methods parameters
           let finalParameters = [];
