096d12d1fe9fa28dce190e52597539b5b7dcc37e
Bart Vanderstukken <bart.vanderstukken@flexmail.be>
2021-03-11 18:11:02 +0100

    Fix double semicolon
    If there is no statusText => don't prepend status with ':'

--- src/components/api-request.js
+++ src/components/api-request.js
@@ -1219,7 +1219,7 @@ export default class ApiRequest extends LitElement {
       const tryResp = await fetch(fetchUrl, fetchOptions);
       tryBtnEl.disabled = false;
       me.responseStatus = tryResp.ok ? 'success' : 'error';
-      me.responseMessage = `${tryResp.statusText}:${tryResp.status}`;
+      me.responseMessage = tryResp.statusText ? `${tryResp.statusText}:${tryResp.status}` : tryResp.status;
       me.responseUrl = tryResp.url;
       tryResp.headers.forEach((hdrVal, hdr) => {
         me.responseHeaders = `${me.responseHeaders}${hdr.trim()}: ${hdrVal}\n`;
