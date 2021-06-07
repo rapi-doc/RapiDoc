7130f76bc1d2ea04484e2c75140503b00b0c9123
Leonid Tatarchuk <Leonid.Tatarchuk@portaone.com>
2021-06-07 09:08:20 +0000

    #494 Add option show-examples

--- src/templates/callback-template.js
+++ src/templates/callback-template.js
@@ -37,6 +37,7 @@ export default function callbackTemplate(callbacks) {
                       schema-description-expanded = "${this.schemaDescriptionExpanded}"
                       allow-schema-description-expand-toggle = "${this.allowSchemaDescriptionExpandToggle}",
                       schema-hide-read-only = "${this.schemaHideReadOnly}"
+                      show-examples = "${this.showExamples}"
                       fetch-credentials = "${this.fetchCredentials}"
                       exportparts="btn btn-fill btn-outline btn-try"
                     > </api-request>
@@ -51,6 +52,7 @@ export default function callbackTemplate(callbacks) {
                       active-schema-tab = "${this.defaultSchemaTab}"
                       schema-expand-level = "${this.schemaExpandLevel}"
                       schema-description-expanded = "${this.schemaDescriptionExpanded}"
+                      show-examples = "${this.showExamples}"
                       allow-schema-description-expand-toggle = "${this.allowSchemaDescriptionExpandToggle}"
                       exportparts = "btn--resp btn-fill--resp btn-outline--resp"
                     > </api-response>
--- src/templates/expanded-endpoint-template.js
+++ src/templates/expanded-endpoint-template.js
@@ -87,6 +87,7 @@ export function expandedEndpointBodyTemplate(path, tagName = '') {
         schema-description-expanded = "${this.schemaDescriptionExpanded}"
         allow-schema-description-expand-toggle = "${this.allowSchemaDescriptionExpandToggle}"
         schema-hide-read-only = "${this.schemaHideReadOnly}"
+        show-examples = "${this.showExamples}"
         fetch-credentials = "${this.fetchCredentials}"
         exportparts = "btn btn-fill btn-outline btn-try"
       > </api-request>
@@ -104,6 +105,7 @@ export function expandedEndpointBodyTemplate(path, tagName = '') {
         schema-description-expanded = "${this.schemaDescriptionExpanded}"
         allow-schema-description-expand-toggle = "${this.allowSchemaDescriptionExpandToggle}"
         schema-hide-write-only = "${this.schemaHideWriteOnly}"
+        show-examples = "${this.showExamples}"
         selected-status = "${Object.keys(path.responses || {})[0] || ''}"
         exportparts = "btn--resp btn-fill--resp btn-outline--resp"
       > </api-response>
