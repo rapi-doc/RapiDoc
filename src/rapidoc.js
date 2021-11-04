93641981bc49b6abe599b4a513ffc1fa3ce53a31
Ekin Dursun <ekindursun@gmail.com>
2021-11-04 16:12:18 +0000

    Wait update before sending spec-loaded
    The documentation says that spec-loaded is sent after the rendering is
    complete, but the current implementation only requests an update, then
    fires the event without waiting its completion.
    
    With this change, all the needed updates are waited until the event is
    dispatched.

--- src/rapidoc.js
+++ src/rapidoc.js
@@ -709,6 +709,8 @@ export default class RapiDoc extends LitElement {
       }
     }
     this.requestUpdate();
+    // eslint-disable-next-line no-await-in-loop
+    while (!await this.updateComplete);
     const specLoadedEvent = new CustomEvent('spec-loaded', { detail: spec });
     this.dispatchEvent(specLoadedEvent);
 
