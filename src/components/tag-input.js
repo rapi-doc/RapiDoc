ef1c212c0e80bb6c71e1bfb1b9a77dccde92a074
Edward Ruchevits <ruchevits@gmail.com>
2021-12-03 04:55:51 +0200

    fix eslint errors

--- src/components/tag-input.js
+++ src/components/tag-input.js
@@ -39,12 +39,10 @@ export default class TagInput extends LitElement {
     const clipboardData = e.clipboardData || window.clipboardData;
     const pastedData = clipboardData.getData('Text');
     const pastedArray = pastedData ? pastedData.split(',').filter((v) => v.trim() !== '') : '';
-    
-    if(pastedArray) {
+    if (pastedArray) {
       if (Array.isArray(this.value)) {
         this.value = [...this.value, ...pastedArray];
-      }
-      else {
+      } else {
         this.value = pastedArray;
       }
     }
