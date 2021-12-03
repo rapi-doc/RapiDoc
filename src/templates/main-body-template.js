bef5674c5f0cdd116f9d66947edf8e0862b7a9ab
Edward Ruchevits <ruchevits@gmail.com>
2021-12-03 16:43:03 +0200

    recover persisted api keys on all pages

--- src/templates/main-body-template.js
+++ src/templates/main-body-template.js
@@ -6,7 +6,7 @@ import focusedEndpointTemplate from '~/templates/focused-endpoint-template';
 import overviewTemplate from '~/templates/overview-template';
 import endpointTemplate from '~/templates/endpoint-template';
 import serverTemplate from '~/templates/server-template';
-import securitySchemeTemplate from '~/templates/security-scheme-template';
+import securitySchemeTemplate, { recoverPersistedApiKeys } from '~/templates/security-scheme-template';
 import headerTemplate from '~/templates/header-template';
 import navbarTemplate from '~/templates/navbar-template';
 import advancedSearchTemplate from '~/templates/advance-search-template';
@@ -16,6 +16,9 @@ import { isValidHexColor } from '~/utils/color-utils';
 export default function mainBodyTemplate(isMini = false, showExpandCollapse = true, showTags = true, pathsExpanded = false) {
   if (!this.resolvedSpec) {
     return '';
+  }
+  if (this.persistAuth === 'true') {
+    recoverPersistedApiKeys.call(this);
   }
   const newTheme = {
     bg1: isValidHexColor(this.bgColor) ? this.bgColor : '',
