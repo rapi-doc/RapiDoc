1f4767b7e18bb44e29796e9d3bb1222ca9d67a6c
drafter500 <vzemtsov@yahoo.com>
2022-03-02 11:26:31 +0100

    UP-2685 - Adjusts styles for expanded response

--- src/styles/custom-styles.js
+++ src/styles/custom-styles.js
@@ -6,5 +6,22 @@ customize their theme. Simply add your css to this file and yarn build.
 */
 
 export default css`
+  .tree {
+    background-color: var(--color-ground-dark);
+  }
 
+  .expanded-endpoint-body > * {
+    width: 50%;
+  }
+
+  .expanded-req-resp-container {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: calc(var(--base-gap) * 2);
+  }
+
+  .m-btn {
+    border-radius: 1000px;
+  }
 `;
