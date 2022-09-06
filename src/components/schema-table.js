42fdc1b664f9e6fd44754bca0fff5e7e5a86077c
Bart Vanderstukken <bart.vanderstukken@socialseeder.com>
2022-09-06 16:14:05 +0200

    Add support for `const`
    This is allowed in OA 3.1
    Cfr https://datatracker.ietf.org/doc/html/draft-bhutton-json-schema-validation-00#section-6.1.3

--- src/components/schema-table.js
+++ src/components/schema-table.js
@@ -312,7 +312,7 @@ export default class SchemaTable extends LitElement {
           ${dataType === 'array' ? html`<span class="m-markdown-small">${unsafeHTML(marked(description))}</span>` : ''}
           ${constraint ? html`<div style='display:inline-block; line-break:anywhere; margin-right:8px;'> <span class='bold-text'>Constraints: </span> ${constraint}</div>` : ''}
           ${defaultValue ? html`<div style='display:inline-block; line-break:anywhere; margin-right:8px;'> <span class='bold-text'>Default: </span>${defaultValue}</div>` : ''}
-          ${allowedValues ? html`<div style='display:inline-block; line-break:anywhere; margin-right:8px;'> <span class='bold-text'>Allowed: </span>${allowedValues}</div>` : ''}
+          ${allowedValues ? html`<div style='display:inline-block; line-break:anywhere; margin-right:8px;'> <span class='bold-text'>${type === 'const' ? 'Value' : 'Allowed'}: </span>${allowedValues}</div>` : ''}
           ${pattern ? html`<div style='display:inline-block; line-break:anywhere; margin-right:8px;'> <span class='bold-text'>Pattern: </span>${pattern}</div>` : ''}
         </div>
       </div>
