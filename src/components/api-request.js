7cf5e1b60d029d20bf55b9fd7b45addce87e86aa
Misfits09 <nathan.perriolat@fifteen.eu>
2023-05-19 16:00:38 +0200

    add ctrl+enter shortcut to fire a request

--- src/components/api-request.js
+++ src/components/api-request.js
@@ -692,6 +692,12 @@ export default class ApiRequest extends LitElement {
                       const requestPanelEl = this.getRequestPanel(e);
                       this.liveCURLSyntaxUpdate(requestPanelEl);
                     }}
+                    @keydown=${(e) => {
+                      if ((e.keyCode === 10 || e.keyCode === 13) && e.ctrlKey) {
+                        return this.onTryClick(e);
+                      }
+                    }}
+
                   ></textarea>
                 </div>  
               `)}
