2a0ed25c79db8a0ce32ddead33f79a1a9fb5b18f
Jerome Saint-Clair <saint-clair@spideo.tv>
2024-01-02 11:24:51 +0100

    Return unsorted tags

--- src/utils/spec-parser.js
+++ src/utils/spec-parser.js
@@ -373,6 +373,7 @@ function groupByTags(openApiSpec, sortEndpointsBy, generateMissingTags = false, 
     }); // End of Methods
   }
 
+  /*
   const tagsWithSortedPaths = tags.filter((tag) => tag.paths && tag.paths.length > 0);
   tagsWithSortedPaths.forEach((tag) => {
     if (sortEndpointsBy === 'method') {
@@ -387,4 +388,6 @@ function groupByTags(openApiSpec, sortEndpointsBy, generateMissingTags = false, 
     tag.firstPathId = tag.paths[0].elementId;
   });
   return sortTags ? tagsWithSortedPaths.sort((a, b) => a.name.localeCompare(b.name)) : tagsWithSortedPaths;
+  */
+  return tags;
 }
