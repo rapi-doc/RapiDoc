8269b5f63cc04756e595871dc0f724cd36768e06
Mrin <mrin9@yahoo.com>
2024-09-20 13:42:55 -0400

    fixes  #930 - advance search should consider summary and description both

--- src/utils/common-utils.js
+++ src/utils/common-utils.js
@@ -90,7 +90,7 @@ export function advancedSearch(searchVal, allSpecTags, searchOptions = []) {
         stringToSearch = path.path;
       }
       if (searchOptions.includes('search-api-descr')) {
-        stringToSearch = `${stringToSearch} ${path.summary || path.description || ''}`;
+        stringToSearch = `${stringToSearch} ${path.summary || '' + path.description || ''}`;
       }
       if (searchOptions.includes('search-api-params')) {
         stringToSearch = `${stringToSearch} ${path.parameters?.map((v) => v.name).join(' ') || ''}`;
