da85b8525df26b5555ff0cb026969f9798c47d43
Mrin <mrin9@yahoo.com>
2024-09-23 09:51:46 -0400

    fixes #1026 - remove extra dropdown provided for server selection in the api-request

--- src/components/api-request.js
+++ src/components/api-request.js
@@ -1090,18 +1090,8 @@ export default class ApiRequest extends LitElement {
   }
 
   apiCallTemplate() {
-    let selectServerDropdownHtml = '';
-
-    if (this.servers && this.servers.length > 0) {
-      selectServerDropdownHtml = html`
-        <select style="min-width:100px;" @change='${(e) => { this.serverUrl = e.target.value; }}'>
-          ${this.servers.map((v) => html`<option value = "${v.url}"> ${v.url} - ${v.description} </option>`)}
-        </select>
-      `;
-    }
     const selectedServerHtml = html`
       <div style="display:flex; flex-direction:column;">
-        ${selectServerDropdownHtml}
         ${this.serverUrl
           ? html`
             <div style="display:flex; align-items:baseline;">
