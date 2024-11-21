1c5266a4f200d9c1957854dd51dfe4e73fb3b387
Mrin <mrin9@yahoo.com>
2024-11-21 12:37:59 -0500

    improve example card listing by enabeling support for wide cards

--- docs/src/data/example-list.ts
+++ docs/src/data/example-list.ts
@@ -9,6 +9,7 @@ interface ExampleItem {
   img?: string;
   description?: string;
   subExamples?: SubExampleList[];
+  isWide?: boolean;
   isDoc?: boolean;
 }
 
@@ -25,6 +26,5 @@ import yaml from 'js-yaml';
 import fs from 'fs';
 
 const exampleListData = yaml.load(fs.readFileSync('./docs/src/data/example-list.yaml', 'utf8')) as ExampleList;
-
 export { exampleListData };
 export type { ExampleList, ExampleItem, SubExampleList, SubExample };
